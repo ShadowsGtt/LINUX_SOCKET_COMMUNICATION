26faee393b3bf3b2a2831bbf98f27c32e217b6fc
ShadowsGtt <1025814447@qq.com>
2018-01-22 13:13:55 +0800

    UDP回射服务(服务器程序)

--- UDP/udp_echo_ser.c
+++ UDP/udp_echo_ser.c
@@ -2,12 +2,32 @@
 int main()
 {
     int sockfd = Socket(UDPSERV);
+    if(sockfd == -1)
+    {
+        printf("Socket() failed\n");
+        exit(-1);
+    }
     char recvbuf[300] = "\0";
     int client_len;
     while(1)
     {
+        bzero(recvbuf,sizeof(recvbuf));
         int  n = recvfrom(sockfd,recvbuf,sizeof(recvbuf),0,(struct sockaddr *)&client_addr,&client_len);
-        sendto(sockfd,recvbuf,n,0,(struct sockaddr *)&client_addr,sizeof(client_addr) );
+        if(n == -1)
+        {
+            perror("recvfrom");
+            exit(-1);
+        }
+        recvbuf[n] = 0;
+        printf("client_len:%d\n",client_len);
+        printf("ip:%s\tport:%d\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+        printf("recv %d byte:%s\n",n,recvbuf);
+        int sd = sendto(sockfd,recvbuf,n,0,(struct sockaddr *)&client_addr,client_len);
+        if(sd == -1)
+        {
+            perror("send");
+        }
     }
     return 0;
 }
+
