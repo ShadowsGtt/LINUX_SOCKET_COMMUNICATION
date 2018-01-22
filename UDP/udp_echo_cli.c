6143829804d3afe7945c0a6709a27c75e0682f61
ShadowsGtt <1025814447@qq.com>
2018-01-22 13:15:47 +0800

    UDP客户端使用connect函数(客户端程序)

--- UDP/udp_echo_cli.c
+++ UDP/udp_echo_cli.c
@@ -1,29 +1,35 @@
 #include "gt.h"
 int main()
 {
-    int sockfd = Socket(UDPCLIENT);
     char sendbuf[300] = "\0";
     char recvbuf[300] = "\0";
-    
+    int sockfd = Socket(UDPCLIENT);
+    if(sockfd == -1)
+    {
+        printf("Socket() failed\n");
+        exit(-1);
+    }
+    if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) == -1 )
+    {
+        perror("connect");
+        exit(-1);
+    }
     while(1)
     {
+        bzero(recvbuf,sizeof(recvbuf));
+        bzero(sendbuf,sizeof(sendbuf));
         if(fgets(sendbuf,sizeof(sendbuf),stdin) != NULL)
         {
-            //sendto(sockfd,sendbuf,sizeof(sendbuf),0,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
-            if(send(sockfd,sendbuf,sizeof(sendbuf),0) == -1 )
-            {
-                perror("send");
-                exit(-1);
-            }
-            int n = recv(sockfd,recvbuf,sizeof(recvbuf),0);
-            if(n == -1)
-            {
-                perror("recv");
-                exit(-1);
-            }
-            recvbuf[n] = 0;
-            printf("recv:%s\n",recvbuf);
+            send(sockfd,sendbuf,sizeof(sendbuf),0);
         }
+        int n = recv(sockfd,recvbuf,sizeof(recvbuf),0);
+        if(n == -1)
+        {
+            perror("recv");
+            exit(-1);
+        }
+        recvbuf[n] = 0;
+        printf("recv %d byte:%s\n",n,recvbuf);
     }
     
     return 0;
